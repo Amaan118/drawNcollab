ba066764978c1429912d35793a7cdc45d1fdf629
Mohammad Amaan Khan <2019bcs118@sggs.ac.in>
2022-10-08 23:32:15 +0530

    Added scripts for Google Auth

---
+++ public/javascript/signupGoogleAuth.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVFBpY6xUuy-h2PRgyYKyHI8McD6dXw-M",
+    authDomain: "drawncollab.firebaseapp.com",
+    projectId: "drawncollab",
+    storageBucket: "drawncollab.appspot.com",
+    messagingSenderId: "234353709129",
+    appId: "1:234353709129:web:2427d1b819fcd0f69fef98",
+    measurementId: "G-N8Z9MKR99R"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signupWithGoogle = () => {
+    signInWithPopup(auth, provider).then(async (result) => {
+        const [firstname, lastname] = result.user.displayName.split(" ");
+        const email = result.user.email;
+        const profileUrl = result.user.photoURL;
+
+        const data = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "profile": profileUrl,
+        }
+
+        await axios.post("/signup/google", data);
+        window.location.replace("http://localhost:3000/draw");
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+
+const googleLogin = document.getElementById("google-login");
+googleLogin.addEventListener("click", () => {
+    signupWithGoogle();
+});
